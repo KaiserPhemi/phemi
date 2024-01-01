387eff7b76027333ebb353b59b14ff09131f1e2e
Oluwafemi Akinwa <kaiser.phemi@protonmail.ch>
2024-01-01 18:23:14 +0100

    chore()
    - intialise project

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,10 +8,11 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: '/phemi',
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Personal Site`,
+    description: `My personal site`,
+     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
